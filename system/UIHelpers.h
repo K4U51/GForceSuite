fa01586f4b7faaf3582d073ae7a9e49120cfe196
Kaus1 <dallabillgates@gmail.com>
2025-10-17 09:40:12 -0700

    Create UIHelpers.h with swipe and tap detection
    Added UIHelpers namespace with swipe and tap detection stubs.

---
+++ system/UIHelpers.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <Arduino.h>
+
+namespace UIHelpers {
+  bool detectSwipe() {
+    // TODO: Add capacitive/touch swipe detection logic
+    return false;
+  }
+
+  bool detectTap() {
+    // TODO: Detect simple tap for Touch Timer or reset events
+    return false;
+  }
+}
